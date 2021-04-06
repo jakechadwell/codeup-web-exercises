0c12c065b8d9bbf5d196f9838c713643cc474a9a
Jake Chadwell <jake.chadwell01@gmail.com>
2021-04-06 13:56:30 -0500

    completed mouse events exercise

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,5 +1,15 @@
 
 $(document).ready(function() {
-    $('h1').css('color', 'blue');
+    $('h1').click(function() {
+        $(this).css('background-color', 'rebeccapurple');
+    });
+    $('p').click(function() {
+        $(this).css('font-size', '18px');
+    });
+    $('li').hover(function () {
+        $(this).css('color', 'red');
+    }, function () {
+        $(this).css('color', 'black');
+    });
 });
 
