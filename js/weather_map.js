7ab0fb09872fa9d32342c528e755888ad77bfb5e
Jake Chadwell <jake.chadwell01@gmail.com>
2021-04-14 21:52:38 -0500

    completed weather app project

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,72 +1,86 @@
 "use strict";
-
+$(document).ready(function () {
 mapboxgl.accessToken = mapboxToken;
-
+var coord = [32.7555, -97.3308];
 var weatherMap = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/jakechadwell/cknf0f8s64aei17oqvg703v98', // style URL
-    center: [-97.3308, 32.7555], // starting position [lng, lat]
+    center: [coord[1],coord[0]], // starting position [lng, lat]
     zoom: 12 // starting zoom
 });
+var mainMarker = new mapboxgl.Marker({color: 'blue', draggable: true})
+    .setLngLat([coord[1],coord[0]])
+    .addTo(weatherMap)
+console.log('displayed map')
+weather()
 
 
-$(document).ready(function (){
-    $.get('https://api.openweathermap.org/data/2.5/onecall', {
-        lat:32.7555,
-        lon:-97.3308,
-        appid: OPEN_WEATHER_APPID,
-        units: 'imperial',
-    }).done(function (results){
-
-        var mainMarker = new mapboxgl.Marker({color: 'orange', draggable: true})
-            .setLngLat([-97.3308, 32.7555])
-            .addTo(weatherMap)
 
 
-        function dragEnd(){
-            var lngLat = mainMarker.getLngLat();
-            weatherMap.flyTo({
-                center: [lngLat.lng, lngLat.lat]
-            });
+    function dragEnd() {
+        coord = mainMarker.getLngLat();
+        coord = [coord.lat, coord.lng];
+        // weatherMap.flyTo({
+        //     center: [coord.lng, coord.lat]
+        // });
+        weather()
+        console.log(coord);
+    }
 
-            console.log(lngLat);
-        }
-        mainMarker.on('dragend', dragEnd)
-
-
-        function injectHtml(id, data){
-            var string = (data.weather[0].description).toString()
-            var textNode = document.createTextNode(string)
-            var string1 = (data.humidity).toString()
-            var textNode1 = document.createTextNode(string1)
-            var string2 = (data.wind_speed).toString()
-            var textNode2 = document.createTextNode(string2)
-            var string3 = (data.pressure).toString()
-            var textNode3 = document.createTextNode(string3)
-            var iconcode = data.weather[0].icon;
-            var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    mainMarker.on('dragend', dragEnd);
 
 
-            document.getElementById("wicon"+id).setAttribute('src', iconurl);
-            document.getElementById(id).children[0].children[0].innerHTML = (new Date(data.dt * 1000)).toDateString()
-            document.getElementById(id).children[1].children[0].innerHTML = "Temp: " + data.temp.min + "&deg;/" + data.temp.max + "&deg;";
-            document.getElementById(id).children[1].children[3].children[0].appendChild(textNode);
-            document.getElementById(id).children[1].children[3].children[1].appendChild(textNode1);
-            document.getElementById(id).children[1].children[3].children[2].appendChild(textNode2);
-            document.getElementById(id).children[1].children[3].children[3].appendChild(textNode3);
 
-        }
-        function createCards() {
-            var apiCallData = results.daily;
-            for (var i = 0; i < 5; i++) {
-                injectHtml(i, apiCallData[i]);
-            }
-        }
-        createCards();
+    function weather() {
+        $.get('https://api.openweathermap.org/data/2.5/onecall', {
+            lat: coord[0],
+            lon: coord[1],
+            appid: OPEN_WEATHER_APPID,
+            units: 'imperial',
+        }).done(function (results) {
         console.log(results)
-        console.log(new Date(results.daily[0].dt*1000))
-    })
-});
+
+
+
+            function injectHtml(id, data) {
+                var iconcode = data.weather[0].icon;
+                var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+                var currentLocation = reverseGeocode({lat: coord[0], lng: coord[1]}, mapboxToken).then(function (results){
+                    weatherMap.flyTo({
+                        center: [coord[1], coord[0]]
+                    });
+                    var place = (results.features[0].place_name).split(",")
+                    document.getElementById("current-location").innerHTML = "Current Location: " + place[1] + ", " + place[2]
+                    console.log(results.features[0].place_name);
+                    console.log(place[1] + ", " + place[2]);
+                });
+                // document.getElementById('current-location').appendChild(textNode4)
+
+                document.getElementById("wicon" + id).setAttribute('src', iconurl);
+                document.getElementById(id).children[0].children[0].innerHTML = (new Date(data.dt * 1000)).toDateString()
+                document.getElementById(id).children[1].children[0].innerHTML = "Temp: " + data.temp.min + "&deg;/" + data.temp.max + "&deg;";
+                document.getElementById(id).children[1].children[3].children[0].innerHTML = "Description: " + data.weather[0].description;
+                document.getElementById(id).children[1].children[3].children[1].innerHTML = "Humidity: " + data.humidity;
+                document.getElementById(id).children[1].children[3].children[2].innerHTML = "Wind: " + data.wind_speed;
+                document.getElementById(id).children[1].children[3].children[3].innerHTML = "Pressure " + data.pressure;
+
+            }
+
+            function createCards() {
+                var apiCallData = results.daily;
+                for (var i = 0; i < 5; i++) {
+                    injectHtml(i, apiCallData[i]);
+                }
+            }
+
+            createCards();
+
+        })
+    }
+
+})
+
+
 
 
 
